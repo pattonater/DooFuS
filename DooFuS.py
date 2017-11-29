dbd18ac417eaaad7a92bba9b2d5e63c157649c5e
pattonater <rjp2@williams.edu>
2017-11-28 20:12:41 -0500

    Close connection with node on failed heartbeat, correct handling of new connections

--- DooFuS.py
+++ DooFuS.py
@@ -7,12 +7,11 @@ import Node
 
 
 
-PORT = 8875 
+PORT = 8876 
 _listen = None
 _nodes = {}
 myip = "137.165.163.84"
-#_ips = ["137.165.163.84", "137.165.160.208", "137.165.162.149", "137.165.175.204"]
-_ips = ["137.165.160.208", "137.165.162.149", "vaca.cs.williams.edu", "137.165.175.204"]
+_ips = ["137.165.163.84", "137.165.160.208", "137.165.162.149", "137.165.175.204"]
 
 def add_node(ip):
     try:
@@ -41,7 +40,7 @@ def connect_to_network():
     print("Connecting to network...")
     for ip in _ips:
         # TODO make this check if is your own or does it matter will just fail
-        not_mine = True
+        not_mine = not ip == myip 
         if not_mine:
             # TODO select a free port?
             add_node(ip)
@@ -51,19 +50,27 @@ def send_heartbeats():
     while True:
         time.sleep(5)
         for node in _nodes.values():
-            node.send_heartbeat()
-            print("Hearbeat sent to " + str(node._ip))
+
+            if node.is_alive():
+                success = node.send_heartbeat()
+                
+                if success:
+                    print("Hearbeat sent to " + str(node._ip))
+                else:
+                    print("Node " + str(node._ip) + " not found. Disconnecting")
+                    node.close_connection()
 
 
 def listen_for_messages(conn, ip):
+    conn.setblocking(True)
     print("Listening to " + str(ip))
     while True:
         msg = conn.recv(1024)
-        print("Message: " + str(msg) + " from " + str(ip))
+        #print("Message: " + str(msg) + " from " + str(ip))
 
         if msg == b"H":
             print("Recieved heartbeat from " + str(ip))
-            _nodes[ip].update_pulse()
+            _nodes[ip].record_pulse()
             
 if __name__ == "__main__":
 
@@ -71,11 +78,7 @@ if __name__ == "__main__":
     print("Starting up")
 
     listen = socket.socket()
-    #host = socket.gethostname()
-    host = myip #socket.gethostbyname('localhost')
-
-    #print(myip)
-    #print(socket.gethostbyname(socket.gethostname()))
+    host = myip 
     
     listen.bind((host, PORT))
     listen.listen()
@@ -90,11 +93,11 @@ if __name__ == "__main__":
         ip = addr[0]
 
         print("Contacted by node at " + str(ip))
-        add_node(ip)
 
-        threading.Thread(target=listen_for_messages, args=(conn, addr,)).start()
+        if ip not in _nodes:
+            add_node(ip)
+            print("This is a new node")
+        else:
+            _nodes[ip].set_connection(conn)
 
-
-    
-
-    
+        threading.Thread(target=listen_for_messages, args=(conn, ip,)).start()
--- Node.py
+++ Node.py
@@ -13,16 +13,26 @@ class Node:
         self._last_pulse = pulse
 
 
-    def update_pulse(self):
+    def record_pulse(self):
         # TODO set to now maybe
         self._last_pulse = time.time()
-
 
     def is_alive(self):
         return time.time() - self._last_pulse < TIMEOUT
 
+    def close_connection(self):
+        self._conn.close()
+
+    def set_connection(self, conn):
+        self._conn = conn
+        self.record_pulse()
 
     def send_heartbeat(self):
-        self._conn.send(b"H")
+        try:
+            self._conn.send(b"H")
+        except:
+            return False
+
+        return True
         
 
