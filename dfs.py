e8181b23620cea4da337319645a7cfc806011d5e
Eli Meckler <ecm3@williams.edu>
2017-11-29 23:25:17 -0500

    Little exception handling thing

--- dfs.py
+++ dfs.py
@@ -14,8 +14,11 @@ class DFS:
     def __init__(self, log_name = None):
         self._log_name = log_name if log_name else "dfs.json"
 
-        with open(self._log_name, 'r') as file:
-            self._log = json.load(file)        
+        try:
+            with open(self._log_name, 'r') as file:
+                self._log = json.load(file)
+        except IOError as e:
+            print("Failed to read log. Error:\n" + e)
 
     def _update():
         try:
