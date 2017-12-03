b9bc3141dce3d481d1a62cbd02c130ded38a5f95
Eli Meckler <ecm3@williams.edu>
2017-12-02 19:04:24 -0500

    DFS: 	- Thread-safety around json object manipulation and file i/o
    test.py:
    	- Small clarity fixes in DFS module

--- dfs.py
+++ dfs.py
@@ -6,8 +6,10 @@
 #  _log: json object read from file at startup, updated by all operations
 #  _UPDATE_PERIOD: how many times update needs to be called between disk writes
 #  _current_update: number of updates since last disk write
+#  _lock: lock 
 
-import json
+import json                 # _log, file i/o
+from threading import Lock  # _lock
 
 ###########################
 ## DFS Class
@@ -20,9 +22,11 @@ class DFS:
         # fairly expensive for big file systems. When it's set to 1, it will
         # update to disk every time _update() is called.
         self._UPDATE_PERIOD = update_period if update_period else 1
-
-        self._log_name = log_name if log_name else "dfs.json"
         self._current_update = 0
+        # Input json file to read dfs from
+        self._log_name = log_name if log_name else "dfs.json"
+        # For thread safety
+        self._lock = Lock()
 
         try:
             with open(self._log_name, 'r') as file:
@@ -34,10 +38,12 @@ class DFS:
     # Takes the current json instance and writes it back to disk.
     # This should be called with some regularity, but not necessarily
     # after every operation. Frequency controlled by self._UPDATE_PERIOD
-    def _update(self):
+    def _update(self, toFile=False):
         # Early abort if we don't want to write to disk yet
-        self._current_update += 1
-        if self._current_update < self._UPDATE_PERIOD:
+        if not toFile:
+            self._current_update += 1
+
+        if self._current_update < self._UPDATE_PERIOD and not toFile:
             return
 
         # Time to write to disk
@@ -48,11 +54,13 @@ class DFS:
             raise DFSIOError(e)
 
         # Reset disk write time track
-        self._current_update = 0            
+        self._current_update = self._current_update if toFile else 0          
 
 
     # Adds file object to _log
     def add_file(self, filename, uploader):
+        self._lock.acquire()
+
         # Verify the file doesn't already exist (name collision)
         for f in self._log["files"]:
             if f["filename"] == filename and f["uploader"] == uploader:
@@ -66,9 +74,12 @@ class DFS:
 
         self._update()
 
+        self._lock.release()
         
     # Removes file object from _log
     def delete_file(self, filename):
+        self._lock.acquire()
+
         initial_file_count = len(self._log["files"]) 
         self._log["files"][:] = [f for f in self._log["files"]
                                  if f.get("filename") != filename]
@@ -78,6 +89,13 @@ class DFS:
 
         self._update()
 
+        self._lock.release()
+
+    # Forces a write to disk
+    def update_disk():
+        self._lock.acquire()
+        self._update(True)
+        self._lock.release()
 
 ###########################
 ## DFS Exceptions
