b9435860a96894d3aff8cc5e1c278758ccbed032
Eli Meckler <ecm3@williams.edu>
2017-12-12 13:59:57 -0500

    Some desperate changes no time for commit messages

--- doofus.py
+++ doofus.py
@@ -137,7 +137,7 @@ def listen_for_messages(conn, host):
                     handle_upload(msg, host)
 
 def handle_request_file(msg, host):
-    msglist = msg.split(Message.DELIMITER)
+    msg = msg.split(Message.DELIMITER)
     file_name = msg[0]
     part_num = msg[1]
     total_parts = msg[2]
@@ -146,7 +146,7 @@ def handle_request_file(msg, host):
     network.serve_file_request(file_name, part_num, total_parts, host, file)
 
 def handle_store_replica(msg, host):
-    msglist = msg.split(Message.DELIMITER)
+    msg = msg.split(Message.DELIMITER)
     file_name = msg[0]
     uploader = msg[1]
     part_num = msg[2]
@@ -159,7 +159,7 @@ def handle_store_replica(msg, host):
     logger.info("Finished alerting other nodes in network")
     
 def write_slice(msg):
-    msglist = msg.split(Message.DELIMITER)
+    msg = msg.split(Message.DELIMITER)
     filename = msg[0]
     part = msg[1]
     total = msg[2]
@@ -238,6 +238,7 @@ def user_interaction():
             add_file(file)
         elif text.startswith("download"):
             file = text[9:]
+            manager.download_file(file)
             # tell dfs manager to download
         elif text == "files":
             print_file_list()
